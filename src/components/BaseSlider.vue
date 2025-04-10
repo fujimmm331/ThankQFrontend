1b652f24cd68e8f686388f67d1417939dd118a66
fujimmm331 <nyappy15th@gmail.com>
2025-04-10 11:27:09 +0900

    feat: init

---
+++ src/components/BaseSlider.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const progress = ref(40);
+
+</script>
+
+<template>
+  <input
+    type="range"
+    min="0"
+    max="100"
+    v-model="progress"
+    class="range" />
+</template>
+
---
+++ src/components/ThSlider.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const model = defineModel({ default: 0 })
+
+</script>
+
+<template>
+  <input type="range"
+         min="0"
+         max="100"
+         v-model="model"
+         class="range text-#D2B48C [--range-progress:#D2B48C] [--range-bg:black] [--range-thumb:black] border border-black border-dashed w-full rounded-lg"
+         style="border-image: repeating-linear-gradient(to right, #6b7280 0, #6b7280 10px, transparent 10px, transparent 20px) 1; z-index: 1px;" />
+</template>
+
