d373500aa2664ae52e0a04df9da1385bf9f9e7da
fujimmm331 <nyappy15th@gmail.com>
2025-04-10 12:40:45 +0900

    feat: vue, vue-use, vue-routerのautoImportを設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core', // ← ここで VueUse を追加！
+      ],
+    }),
   ],
   resolve: {
     alias: {
