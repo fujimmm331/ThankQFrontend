388c0057e3c9d437ca3dea1651c40d210771d3fc
fujimmm331 <nyappy15th@gmail.com>
2025-04-21 11:14:18 +0900

    feat: ラジオボタンコンポーネント作成

---
+++ src/components/Common/BaseRadio/BaseRadio.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { BaseRadioProps } from './Type';
+
+defineProps<BaseRadioProps>();
+
+</script>
+
+<template>
+  <label
+    class="flex justify-between border-1 p-4 radio-primary
+      border-primary rounded-md bg-[#E2F6ED]"
+  >
+    {{ label }}
+    <input
+      class="radio"
+      name="radio-1"
+      type="radio"
+    >
+  </label>
+</template>
