d0a2adb8fe14b9508de7196bcbf94c21dec2f6e8
fujimmm331 <nyappy15th@gmail.com>
2025-04-21 11:14:05 +0900

    linkコンポーネント作成

---
+++ src/components/Common/BaseLink/BaseLink.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { ThankQColorType } from '@/themes/color';
+import type { BaseTextProps } from './Type';
+
+const props = defineProps<BaseTextProps>();
+
+const btnColor = computed(() => {
+  const colorClass: Record<ThankQColorType, string> = {
+    'primary': 'link-primary',
+    'secondary': 'link-secondary',
+    'accent': 'link-accent',
+    'info': 'link-info',
+    'success': 'link-success',
+    'warning': 'link-warning',
+    'error': 'link-error'
+  }
+  return props.color ? colorClass[props.color] : 'link-neutral'
+})
+
+
+</script>
+
+<template>
+  <a
+    class="link"
+    :class="[btnColor]"
+    href="https://daisyui.com/components/link/"
+  >
+    <slot />
+  </a>
+</template>
