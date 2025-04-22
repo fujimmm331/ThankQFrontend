932cc779dbbdc7f65e4ddf8bc49c5d1f5b9204d6
fujimmm331 <nyappy15th@gmail.com>
2025-04-22 14:57:12 +0900

    feat: カルーセルの実装

---
+++ src/components/Common/BaseCarousel/BaseCarouselItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { ImgItemType } from './Type';
+
+defineProps<{
+  imgItem: ImgItemType,
+}>();
+
+
+
+</script>
+
+<template>
+  <div
+    class="carousel-item w-full"
+  >
+    <img
+      class="w-full aspect-square"
+      :src="imgItem.url"
+    >
+  </div>
+</template>
