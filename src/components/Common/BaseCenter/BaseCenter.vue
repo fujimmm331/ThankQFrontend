d6fbc22b2e117c6022db58f0c0a5e4c2a47c27d5
fujimmm331 <nyappy15th@gmail.com>
2025-04-24 22:23:34 +0900

    feat: 中心に配置するコンポーネントを作成

---
+++ src/components/Common/BaseCenter/BaseCenter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { BaseCenterProps } from './Type';
+import BaseStack from '../BaseStack/BaseStack.vue';
+
+const props = withDefaults(defineProps<BaseCenterProps>(), {
+  col: true
+});
+
+const center = computed(() => {
+  if (props.col) {
+    return 'justify-center'
+  }
+
+  return 'items-center'
+})
+
+</script>
+
+<template>
+  <BaseStack
+    :class="[center]"
+    v-bind="$props"
+  >
+    <slot>ここにコンポーネントを挟んでください</slot>
+  </BaseStack>
+</template>
---
+++ src/components/Common/BaseCenter/Type.ts
@@ -0,0 +1,4 @@
+import type { BaseStackProps } from "../BaseStack/Type"
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface BaseCenterProps extends BaseStackProps{}
