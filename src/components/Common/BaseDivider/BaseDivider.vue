ef0ad5373705a3d3b2be8afa6eef98f3234e063c
fujimmm331 <nyappy15th@gmail.com>
2025-04-24 22:00:39 +0900

    feat: カードコンポーネントを作成

---
+++ src/components/Common/BaseDivider/BaseDivider.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="divider base-divider">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.base-divider {
+  height: 0;
+}
+</style>
