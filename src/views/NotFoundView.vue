ea2395f84a82d87666c5a963f95de58a9bd57989
fujimmm331 <nyappy15th@gmail.com>
2025-05-07 20:55:59 +0900

    feat: インベーダーゲームを切り出した

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -4,53 +4,8 @@ import BaseHeading from '@/components/Common/BaseHeading/BaseHeading.vue';
 import BaseSection from '@/components/Common/BaseSection.vue';
 import BaseStack from '@/components/Common/BaseStack/BaseStack.vue';
 import BaseText from '@/components/Common/BaseText/BaseText.vue';
+import InvaderGame from '@/components/InvaderGame/InvaderGame.vue';
 import { useTokenStore } from '@/stores/tokenStore';
-
-interface EnemyType {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  color: string;
-  dx: number;
-}
-
-interface EnemyBulletType {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-  speed: number;
-}
-
-
-const canvasRef = useTemplateRef('canvas');
-
-const player = {
-  x: 320 / 2 - 15,
-  y: 320 - 30,
-  width: 30,
-  height: 15,
-  color: 'white',
-  speed: 10
-};
-
-const bullets: {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}[] = [];
-
-const enemies: EnemyType[] = [];
-const enemyBullets: EnemyBulletType[] = [];
-
-const bulletSpeed = 5;
-
-
-const context = computed(() => {
-  return canvasRef.value?.getContext("2d");
-})
 
 const router = useRouter()
 const store = useTokenStore()
@@ -58,14 +13,6 @@ const store = useTokenStore()
 const hasToken = computed(() => {
   return !!store.token
 })
-
-function onClickRight() {
-  moveRight()
-}
-
-function onClickLeft() {
-  moveLeft()
-}
 
 async function onClick() {
   await router.push({
@@ -75,271 +22,43 @@ async function onClick() {
     }
   })
 }
-
-function onFire() {
-  fire();
-}
-
-function drawPlayer() {
-  if (!context.value) return;
-  context.value.clearRect(0, 0, 320, 320);
-  context.value.fillStyle = player.color;
-  context.value.fillRect(player.x, player.y, player.width, player.height);
-}
-
-function drawBullets() {
-  if (!context.value) return;
-  context.value.fillStyle = 'white';
-  bullets.forEach(bullet => {
-    context.value?.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
-  });
-}
-function drawPixelEnemy(enemy: EnemyType) {
-  const context = canvasRef.value?.getContext('2d')
-  const pixelSize = 5;
-  const sprite = [
-    '00100',
-    '01110',
-    '11111',
-    '10101',
-    '00100',
-    '01010'
-  ];
-  if (!context) return
-
-  context.fillStyle = enemy.color;
-
-  for (let row = 0; row < sprite.length; row++) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    for (let col = 0; col < sprite[row]!.length; col++) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      if (sprite[row]![col] === '1') {
-        context.fillRect(
-          enemy.x + col * pixelSize,
-          enemy.y + row * pixelSize,
-          pixelSize,
-          pixelSize
-        );
-      }
-    }
-  }
-}
-
-function drawEnemies() {
-  for (const enemy of enemies) {
-    drawPixelEnemy(enemy)
-  }
-}
-
-function updateEnemies() {
-  for (const enemy of enemies) {
-    enemy.x += enemy.dx;
-
-    // 端に当たったら反転（簡単な動き）
-    if (enemy.x <= 0 || enemy.x + enemy.width >= (canvasRef.value?.width ?? 320)) {
-      enemy.dx *= -1;
-      // すこし下にずらす
-      // for (const e of enemies) {
-      //   e.y += 10;
-      // }
-      break;
-    }
-  }
-}
-
-function createEnemies(rows: number, cols: number) {
-  const padding = 15;
-  const offsetX = 30;
-  const offsetY = 30;
-  const width = 20;
-  const height = 20;
-
-  for (let row = 0; row < rows; row++) {
-    for (let col = 0; col < cols; col++) {
-      enemies.push({
-        x: offsetX + col * (width + padding),
-        y: offsetY + row * (height + padding),
-        width,
-        height,
-        color: 'green',
-        dx: 1
-      });
-    }
-  }
-}
-
-function enemyShoot() {
-  if (enemies.length === 0) return;
-
-  const shooter = enemies[Math.floor(Math.random() * enemies.length)];
-  if (shooter) {
-    enemyBullets.push({
-      x: shooter.x + shooter.width / 2 - 2,
-      y: shooter.y + shooter.height,
-      width: 4,
-      height: 10,
-      speed: 3
-    });
-  }
-}
-
-function updateEnemyBullets() {
-  for (let i = enemyBullets.length - 1; i >= 0; i--) {
-    const bullet = enemyBullets[i]
-    if (bullet) {
-      bullet.y += bullet.speed;
-
-      // 画面外に出たら消す
-      if (bullet.y > (canvasRef.value?.height ?? 320)) {
-        enemyBullets.splice(i, 1);
-      }
-    }
-  }
-}
-
-function drawEnemyBullets() {
-  const ctx = context.value
-  if (ctx) {
-    ctx.fillStyle = 'red';
-    for (const bullet of enemyBullets) {
-      ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
-    }
-  }
-}
-
-function updateBullets() {
-  for (let i = bullets.length - 1; i >= 0; i--) {
-    const bullet = bullets[i];
-    if (bullet) {
-      bullet.y -= bulletSpeed;
-      if (bullet.y + bullet.height < 0) {
-        bullets.splice(i, 1);
-      }
-    }
-  }
-}
-
-function moveRight() {
-  if (!canvasRef.value) return
-  player.x = Math.min(canvasRef.value.width - player.width, player.x + player.speed);
-  drawPlayer();
-}
-
-function moveLeft() {
-  if (!canvasRef.value) return
-  player.x = Math.min(canvasRef.value.width - player.width, player.x - player.speed);
-  drawPlayer();
-  drawBullets()
-}
-
-function fire() {
-  bullets.push({
-    x: player.x + player.width / 2 - 2,
-    y: player.y,
-    width: 4,
-    height: 10
-  });
-}
-
-function checkCollisions() {
-  for (let i = bullets.length - 1; i >= 0; i--) {
-    const bullet = bullets[i];
-
-    for (let j = enemies.length - 1; j >= 0; j--) {
-      const enemy = enemies[j];
-      if (bullet && enemy) {
-        const hit =
-          bullet.x < enemy.x + enemy.width &&
-          bullet.x + bullet.width > enemy.x &&
-          bullet.y < enemy.y + enemy.height &&
-          bullet.y + bullet.height > enemy.y;
-
-        if (hit) {
-          bullets.splice(i, 1);
-          enemies.splice(j, 1);
-          break;
-        }
-      }
-    }
-  }
-}
-
-function gameLoop() {
-  context.value?.clearRect(0, 0, canvasRef.value?.width ?? 320, canvasRef.value?.height ?? 320);
-  updateBullets();
-  updateEnemies();
-  updateEnemyBullets();
-  checkCollisions();
-  drawPlayer();
-  drawBullets();
-  drawEnemies();
-  drawEnemyBullets();
-  requestAnimationFrame(gameLoop);
-}
-
-
-onMounted(() => {
-  createEnemies(2, 8);
-  gameLoop()
-  setInterval(enemyShoot, 1000);
-})
-
 </script>
 
 <template>
   <BaseSection>
     <BaseStack
-      class="items-center"
+      class="items-center h-full"
       component="div"
-      gap="lg"
+      gap="xl"
     >
-      <canvas
-        ref="canvas"
-        class="bg-primary"
-        height="320"
-        width="320"
-      />
+      <BaseStack
+        class="items-center"
+        component="div"
+        gap="md"
+      >
+        <BaseHeading
+          tag="h1"
+        >
+          ごめんなさい！
+        </BaseHeading>
 
-      <BaseBtn
-        size="md"
-        @click="onClickLeft"
-      >
-        ←
-      </BaseBtn>
+        <BaseText>
+          お探しのページは見つからなかったようです。
+          <span v-if="!hasToken"><br>再度QRコードを読み込んでみてください！</span>
+        </BaseText>
+        <BaseBtn
+          v-if="hasToken"
+          behavior="block"
+          color="primary"
+          size="sm"
+          variant="outline"
+          @click="onClick"
+        >
+          新郎・新婦からのメッセージページに戻る
+        </BaseBtn>
+      </BaseStack>
 
-      <BaseBtn
-        size="md"
-        @click="onFire"
-      >
-        fire
-      </BaseBtn>
-
-      <BaseBtn
-        size="md"
-        @click="onClickRight"
-      >
-        →
-      </BaseBtn>
-      <BaseHeading
-        tag="h1"
-      >
-        ごめんなさい！
-      </BaseHeading>
-
-      <BaseText>
-        お探しのページは見つからなかったようです。
-        <span v-if="!hasToken"><br>再度QRコードを読み込んでみてください！</span>
-      </BaseText>
-      <BaseBtn
-        v-if="hasToken"
-        behavior="block"
-        color="primary"
-        size="sm"
-        variant="outline"
-        @click="onClick"
-      >
-        新郎・新婦からのメッセージページに戻る
-      </BaseBtn>
+      <InvaderGame />
     </BaseStack>
   </BaseSection>
 </template>
